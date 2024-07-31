31baaa60c3fcac2029ee28954c1768f7a87739eb
David Worms <david@adaltas.com>
2024-07-31 18:36:47 +0200

    test: mutate eslint format

--- test/mutate.js
+++ test/mutate.js
@@ -1,9 +1,9 @@
 import should from "should";
 import { mutate } from "../lib/index.js";
 
-describe("mixme.merge", () => {
-  describe("1st arg not object", () => {
-    it("null makes next object immutable", () => {
+describe("mixme.merge", function () {
+  describe("1st arg not object", function () {
+    it("null makes next object immutable", function () {
       const source = { a_key: "a value", b_key: "b value" };
       const result = mutate(null, source);
       source.a_key = "new value";
@@ -11,20 +11,20 @@ describe("mixme.merge", () => {
     });
   });
 
-  describe("2nd arg not object", () => {
-    it("object with string", () => {
+  describe("2nd arg not object", function () {
+    it("object with string", function () {
       const obj1 = { a_key: "a value", b_key: "b value" };
       const obj2 = "b";
       mutate(obj1, obj2).should.eql("b");
     });
 
-    it("object with undefined", () => {
+    it("object with undefined", function () {
       const obj1 = { a_key: "a value", b_key: "b value" };
       const obj2 = undefined;
       mutate(obj1, obj2).should.eql({ a_key: "a value", b_key: "b value" });
     });
 
-    it("object with null", () => {
+    it("object with null", function () {
       const obj1 = { a_key: "a value", b_key: "b value" };
       const obj2 = null;
       should.not.exists(mutate(obj1, obj2));
